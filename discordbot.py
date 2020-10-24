51f214780328f34c564ed21416838f4a5648b2d5
zxdespair <62172428+zxdespair@users.noreply.github.com>
2020-10-24 16:44:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,7 +14,6 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
