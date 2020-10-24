6ba6b1d99f95c3031680833eb61f4c64b5b01c16
zxdespair <62172428+zxdespair@users.noreply.github.com>
2020-10-24 16:53:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,9 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-if not discord.opus.is_loaded():
-    discord.opus.load_opus("heroku-buildpack-libopus")
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
