833e43873087f6f3a6956dcf7b2131250e543060
zxdespair <62172428+zxdespair@users.noreply.github.com>
2020-10-24 17:30:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -76,14 +76,6 @@ async def play(ctx):
     voice_client.play(ffmpeg_audio_source)
 
     await ctx.send("再生しました。")
-    
-@client.event
-async def on_message(message):
-    # 「やあ」というチャットが来た場合のメッセージ
-    if message.content.startswith("やあ"):
-        # 送り主がチャットボット以外なら返事を返す
-        if client.user != message.author:
-            message = "やあ、" + message.author.name
-            await client.send_message(message.channel, message)
+   
             
 bot.run(token)
