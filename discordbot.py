942f7764f178d0513fa9d55399cd9309aa17f865
zxdespair <62172428+zxdespair@users.noreply.github.com>
2020-10-24 18:19:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,7 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -30,7 +31,7 @@ async def ねむい(ctx):
 async def やほ(ctx):
     await ctx.send('やっほー！')
     
-@bot.command()
+@client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
